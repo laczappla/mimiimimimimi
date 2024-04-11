def4cc3fc375ed4b26252602c059f129711ce916
Antonix6x <166395440+Antonix6x@users.noreply.github.com>
2024-04-11 13:53:59 +0200

    funkcja wypełniania tablicy

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Mial
+{
+    static void Main(string[] args)
+    {
+        bool[] liczby = new bool[100];
+        WypelnianieLiczby(liczby);
+    }
+
+    static void WypelnianieLiczby(bool[] tablica)
+    {
+        for(int i = 0; i < tablica.Length; i++)
+        {
+            tablica[i] = true;
+        }
+    }
+}
