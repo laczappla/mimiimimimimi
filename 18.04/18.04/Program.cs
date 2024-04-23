e57385fd7ec404b502bfe592a05872fce54b7de9
ForSureNotReaper <sewerusblack563@gmail.com>
2024-04-23 13:57:26 +0200

    twoezenie_tablic

---
+++ 18.04/18.04/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //wywołaj obiekt tablica
+        Tablice tablice = new Tablice();
+
+        tablice.Powitanie();
+
+        int[] tab = tablice.StworzTablice();
+    }
+}
---
+++ 18.04/18.04/tablice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Tablice
+{
+	public void Powitanie()
+	{
+		Console.WriteLine("witam");
+	}
+
+	public int[] StworzTablice() 
+	{
+		Console.WriteLine("podaj długość tablicy: ");
+		int dlugosc = int.Parse(Console.ReadLine());
+		int[] tablica = new int[dlugosc];
+		for (int i = 0; i < dlugosc; i++)
+		{
+			Console.WriteLine("podaj kolejny element tablicy: ");
+			tablica[i] = int.Parse(Console.ReadLine());
+		}
+		Console.WriteLine();
+		return tablica;
+	}
+}
